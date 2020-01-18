aaaf83508b947e8ba0be470d4f12eb33e8b08542
John Doe <nadia643@gmail.com>
2020-01-18 23:30:35 +0000

    css to main page

---
+++ app.js
@@ -0,0 +1,7 @@
+let userScore = 0;
+let computerScore = 0;
+
+function computerPlay() {
+    const items = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+    const computerChoice = computerPlay[Math.floor(Math.random() * myArray.length)]
+}
