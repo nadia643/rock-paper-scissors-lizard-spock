d9ce43dae312bf1e9c52ffe58b7993b29cfaa70c
John Doe <nadia643@gmail.com>
2020-01-19 22:20:54 +0000

    switch statements for win and lose functions written

--- app.js
+++ app.js
@@ -1,8 +1,103 @@
 let userScore = 0;
 let computerScore = 0;
 
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const lizard_div = document.getElementById("s");
+const spock_div = document.getElementById("sp");
+
 function computerPlay() {
     items = ['rock', 'paper', 'scissors', 'lizard', 'spock']
     return items[Math.floor(Math.random() * items.length)]
 }
+const smallUserWord = "user".fontsize(3).sub();
+const smallCompWord = "comp".fontsize(3).sub();
 
+// const computerChoice = computerPlay();
+
+function convertToCapital(userSelection) {
+    if (userSelection === "rock") return "Rock";
+    if (userSelection === "paper") return "Paper";
+    if (userSelection === "scissors") return "Scissors";
+    if (userSelection === "lizard") return "Lizard";
+    return "Spock";
+}
+function convertToCapital(computerSelection) {
+    if (userSelection === "rock") return "Rock";
+    if (userSelection === "paper") return "Paper";
+    if (userSelection === "scissors") return "Scissors";
+    if (userSelection === "lizard") return "Lizard";
+    return "Spock";
+}
+
+function win(userSelection, computerSelection) {
+    userScore++
+    userScore_span.innerHTML = userScore;
+    result_div.innerHTML = convertToCapital(userSelection) + `${smallUserWord}` +" beats " + convertToCapital(computerSelection) + `${smallCompWord}` + ". You win!"
+}
+
+function lose(userSelection, computerSelection) {
+    computerScore++
+    computerScore_span.innerHTML = computerScore;
+    result_div.innerHTML = convertToCapital(computerSelection) + `${smallCompWord}` + " beats " + convertToCapital(userSelection) + `${smallUserWord}` + ". You lose!"
+}
+
+function draw() {
+    result_div.innerHTML = "Same same. Try again!"
+}
+
+function game(userSelection) {
+    const computerSelection = computerPlay().toLowerCase();
+    switch(userSelection + computerSelection) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+        case "scissorslizard":
+        case "paperspock":
+        case "rocklizard":
+        case "spockrock":
+        case "lizardspock":
+        case "lizardpaper":
+            win(userSelection, computerSelection);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+        case "lizardscissors":
+        case "spockpaper":
+        case "lizardrock":
+        case "spockrock":
+        case "spocklizard":
+        case "paperlizard":
+            lose(userSelection, computerSelection);
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(userSelection, computerSelection);
+            break;
+    }
+}
+console.log(computerPlay());
+
+game();
+
+function main() {
+rock_div.addEventListener('click', function() {
+    game("rock");
+})
+paper_div.addEventListener('click', function() {
+    game("paper");
+})
+scissors_div.addEventListener('click', function() {
+    game("scissors");
+})
+
+};
+
+main();
